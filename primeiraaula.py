63cd254bfb6a1f37e92bff53e0d459e9c5d86003
Brunosoilvalima <limabrunosilvalima@gmail.com>
2023-08-30 13:54:12 -0300

    first commit

---
+++ primeiraaula.py
@@ -0,0 +1,25 @@
+curso = "python"
+ano = 2023
+valor = 1500.00
+
+print ("curso :", curso," custa: R$", valor, "em", ano )
+
+
+
+
+
+codigo = 123
+nome = "Bruno Lima"
+ativo = True
+salario = 2346.15
+
+print("código: %d" % codigo)
+print("nome: %s" % nome)
+print("ativo: %r" % ativo)
+print("salario: %.2f" % salario)
+
+print("código: %d \t\t tipo: %s" % ( codigo, type(codigo)))
+print("nome: %s \t\t tipo: %s" % (nome,type(nome)))
+print("ativo: %r \t\t tipo: %s" (ativo,type(ativo)))
+print("salario: %.2f \t tipo: %s" % (salario, type (salario)))
+
