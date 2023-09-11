c9d95cc844bb719dac178e436042102d63ce3b15
Brunosoilvalima <limabrunosilvalima@gmail.com>
2023-09-11 13:48:21 -0300

    quintaula.py

---
+++ for.py
@@ -0,0 +1,24 @@
+"""n = int(input("informe um valor"))
+
+for t in range(10):
+ r = n*t
+ print(r)"""
+
+
+"""n = int(input("INFORME UM NUMERO"))
+t = 1 
+while ( t<= 10):
+    r = n*t
+    print(r)
+    t = t+1
+print("acabou")"""
+
+"""print("lista de nome: ")
+for nome in {'miguel', 'gabriela', 'milena',}:
+    print(nome)
+
+print('/nlista de numeros:') 
+lista = {1,2,3,4,10}
+
+for numero in lista:
+    print(numero)"""
---
+++ quintala.py
@@ -0,0 +1,109 @@
+"""vetor = {5, 64, 87, 26, 44, 31 }
+for indice in range ( len(vetor)):
+    print(vetor [indice])"""
+
+"""vetor = {5, 64, 87, 26, 44, 31 }
+for item in vetor:
+    print(item)"""
+
+
+'''vetor = {5, 64, 87, 26, 44, 31 }
+for indice in range (5):
+    print(vetor[indice]) '''
+    
+    
+
+"""vetor = [ 'x' , 5, 9.5, 'curso', True]
+for indice in range (len(vetor)):
+    print (vetor [ indice])"""
+
+
+#------------------------------------------------------#-------------------------------------------------------#------------------------------------#--------------------
+
+
+
+ 
+# vetor = [ 10.99, 3, 50.5 -3.5, 15.0 ]
+# for item in vetor:
+#     print(item)
+
+#-----------------------------------------------------------#-----------------------------------------------#-----------------------------------#----------------------------
+
+alunos = [ ] 
+notas = [ ] 
+
+
+
+# for i in range (3):
+#     alunos.append(input("nome do (a) ° aluno (a): "))
+
+#     notas.append(eval(input("digite a nota do (a) :")))
+
+
+# for i in range (3):
+#          print("\n aluno (a): ", alunos [i], ": nota:", notas[i])
+
+
+#----------------------------------------------------------#---------------------------------------
+
+#  alunos = [] 
+#  notas = [] 
+
+#  for i in range (3):
+#      alunos.append(input("nome do(a) "+ str(i + 1) + "° aluno (a): "))
+#      notas.append(eval(input("digite a nota do(a) " + alunos [i] + ": ")))
+
+#  for i in range (3):
+#     print("n aluno (a):", alunos[i], ": nota ", notas [ i ])
+
+
+#-------------------------------------------#---------------------------#----------
+
+# alunos = input("informe o nome do(a) aluno(a):")
+# quantidade = int(input("informe a quantidade de notas que deseja informar :"))
+# soma = 0 
+# notas = [ ]
+
+
+
+
+# quantidade = int(input("informe a quantidade de atividade:"))
+# for i in range(quantidade):
+#      notas.append(eval(input("digite a " + str (i+ 1) +"ª notas")))
+
+#      for i in range(quantidade):
+#           notas.append(eval(input("digite a " + (str(i+1)) + "ª nota ")))
+#      soma = soma + notas[1] 
+
+#      media = soma / quantidade
+
+#      print("\n a media do aluno(a)", alunos, "é de: ", media )
+
+#-------------------------------------------------------------#----------------------------------------#-----------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
+
+
+
+    
